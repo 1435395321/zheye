0e719c2d76dbf28036105d5c228ebf7cd31a5ed7
zhaobinworks <799132922@qq.com>
2020-10-27 18:07:21 +0800

    define Component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png">
-<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="container">
+
+    </div>
 </template>
 
 <script lang="ts">
-import {
-    defineComponent
-} from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default defineComponent({
-    name: 'App',
+    name: "App",
     components: {
-        HelloWorld
-    }
+    },
 });
 </script>
 
