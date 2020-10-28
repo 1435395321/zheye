3565b0852cc5e47e74c7469509fcb111c0958002
zhaobinworks <799132922@qq.com>
2020-10-28 10:54:29 +0800

    columnList 组件完成

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export interface UserProps {
+    isLogin: boolean;
+    id?: number;
+    name?: string;
+}
+export default defineComponent({
+    name:'GlobalHeader',
+    props: {
+        user: {
+            
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
