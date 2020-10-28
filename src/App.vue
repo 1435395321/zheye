a7ead7a232c20c7d5b0b8fb6387d006504901c72
zhaobinworks <799132922@qq.com>
2020-10-28 10:55:01 +0800

    columnList 组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
     <div class="container">
-
+        
+        <ColumnList :list="list"></ColumnList>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const testData: ColumnProps[] = [
+    {
+        id: 1,
+        title: 'test1的专栏',
+        description: '这是test的专栏，游简介',
+        avatar: 'https://img-blog.csdnimg.cn/20190328114500430.png'
+    },
+    {
+        id: 2,
+        title: 'test2的专栏',
+        description: '这是test2的专栏，游简介',
+        avatar: ''
+    },
+    {
+        id: 2,
+        title: 'test2的专栏',
+        description: '这是test2的专栏，游简介',
+        avatar: 'https://img-blog.csdnimg.cn/20190328114500430.png'
+    }
+]
 export default defineComponent({
     name: "App",
     components: {
+        ColumnList
     },
+    setup() {
+        return {
+            list: testData
+        }
+    }
 });
 </script>
 
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,22 +1,31 @@
+/* eslint-disable no-undef */
 <template>
-    <ul>
-        <li v-for="column in list"
-            :key="column.id">
-            <img :src="column.avatar"
-                 :alt="column.title">
-            <h5>{{column.title}}</h5>
-            <p>{{column.description}}</p>
-            <a href="#">进入专栏</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <img
+                        :src="column.avatar"
+                        class="border border-light my-4"
+                        alt="column.title"
+                        style="width:60%;height:100px"
+                    />
+                    <h5 class="card-title">{{ column.title }}</h5>
+                    <p class="card-text text-left">{{ column.description }}</p>
+                    <a href="#" class="btn btn-outline-primary">进入专栏</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+// PropType 接受一个泛型
+import { computed, defineComponent, PropType } from "vue";
 export interface ColumnProps {
     id: number;
     title: string;
-    avatar: string;
+    avatar?: string;
     description: string;
 }
 export default defineComponent({
@@ -27,7 +36,19 @@ export default defineComponent({
             required: true,
         },
     },
-    setup(props) {},
+    setup(props) {
+        // 计算图片为空的
+        const columnList = computed(() => {
+            return props.list.map(e => {
+                if (!e.avatar)
+                    e.avatar = require('@/assets/logo.png');
+                return e;
+            })
+        })
+        return {
+            columnList,
+        };
+    },
 });
 </script>
 
